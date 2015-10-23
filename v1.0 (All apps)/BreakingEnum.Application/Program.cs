969737793820e28108343125ea2a452356c730fc
Joao Matos Silva <kappy@acydburne.com.pt>
2015-10-23 23:09:20 +0100

    Added Sample code

---
+++ v1.0 (All apps)/BreakingEnum.Application/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BreakingEnum.Common;
+using BreakingEnum.Helper;
+
+namespace BreakingEnum.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Application: {0}", typeof(Program).Assembly.GetName().Version);
+            Console.WriteLine("Helper: {0}", typeof(StatusHelper).Assembly.GetName().Version);
+            Console.WriteLine("Common: {0}", typeof(StatusEnum).Assembly.GetName().Version);
+
+            var status = StatusHelper.IsActive("Active");
+            if (status)
+            {
+                Console.WriteLine("Status is active");
+            }
+            else
+            {
+                Console.WriteLine("Status is Disabled");
+            }
+        }
+    }
+}
---
+++ v1.0 (All apps)/BreakingEnum.Helper/StatusHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BreakingEnum.Common;
+
+namespace BreakingEnum.Helper
+{
+    public class StatusHelper
+    {
+        public static bool IsActive(string statusText)
+        {
+            var status = (StatusEnum) Enum.Parse(typeof (StatusEnum), statusText);
+            var isActive = status == StatusEnum.Active;
+            return isActive;
+        }
+    }
+}
